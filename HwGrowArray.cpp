1e40e8553f28da2fd042b6fd8778af422722c145
jsxcool <jsx2017@outlook.com>
2018-10-12 17:52:41 -0400

    double-direction, head-tail list

--- HwGrowArray.cpp
+++ HwGrowArray.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<string>
+#include<cstdlib>
 using namespace std;
 
 class GrowArray {
@@ -39,14 +41,14 @@ public:
     }
 
     void ADD_FRONT(int start, int step, int end) {
-        int quantity = end - start + 1;
+        int quantity = (end - start)/step + 1;
         size += quantity;
-        if(size >= quantity)
+        while(size >= capacity)
             checkGrow();
         for (int i = size; i > quantity; i--) {
             p[i] = p[i - quantity];
         }
-        for(int v=start, j=quantity - 1; v<=end&&j>=0; v += step) {
+        for(int v=start, j=quantity - 1; v<=end; v += step) {
             p[j] = v;
             j--;
         }
@@ -79,16 +81,65 @@ public:
             s << g.p[i] <<' ';
         return s;
     }
-
+    
 };
+
+
+void getPara(string part2, int* a){
+	int i=0;
+	string s;
+	while(part2[i] != ':'){
+		s += part2[i];
+		i++;
+	}
+	a[0] = atoi(&s[0]);
+	string s1;
+	for(i=i+1; part2[i] != ':'; i++){
+		s1 += part2[i];
+	}
+	a[1] = atoi(&s1[0]);
+	i++;
+	string s2;
+	while(i<part2.size()){
+		s2 += part2[i];
+		i++;
+		}
+	a[2] = atoi(&s2[0]);
+}
 
 
 int main() {
     GrowArray a;
-    //a.ADD_FRONT(1,2,10);
+    while(true){
+    	string part1, part2; // for an instruction
+    	cout << ">>> ";
+    	cin >> part1 >> part2;
+    	//cout << part1 << ' ' << part2 << '\n';
+    	if(part1 == "ADD_FRONT"){
+    		int num[3]; 
+    		getPara(part2, num);
+    		int b=num[0], c=num[1], d=num[2];
+    		a.ADD_FRONT(b, c, d);
+    	} else if(part1 == "ADD_BACK"){
+    		int num[3]; 
+    		getPara(part2, num);
+    		int b=num[0], c=num[1], d=num[2];
+    		a.ADD_BACK(b, c, d);
+    		cout << a << '\n';
+    	} else if(part1 == "REMOVE_FRONT"){
+    		int num = atoi(&part2[0]);
+    		a.REMOVE_FRONT(num);
+    	} else if(part1 == "REMOVE_BACK"){
+    		int num = atoi(&part2[0]);
+    		a.REMOVE_BACK(num);
+    	} else{   // OUTPUT must be "OUTPUT a" (part1, part2)
+    		cout << a << '\n';
+    	}
+    }
+    /*a.ADD_FRONT(1,2,10);    1:2:10
     a.ADD_BACK(5, 5, 25);
     cout << a << '\n' ;
     a.REMOVE_FRONT(2);
     a.REMOVE_BACK(2);
-    cout << a;
+    cout << a << '\n' ;*/
 }
--- Linkedlist.cc
+++ Linkedlist.cc
@@ -7,8 +7,10 @@ private:
 	class Node{
 		public:
 			int val;
-			Node* next; 
-			Node(int v=0, Node* n = nullptr): val(v), next(n) {}
+			Node* next;
+			Node* prev; 
+			Node(int v=0, Node* n=nullptr, Node* p=nullptr)
+				: val(v), next(n), prev(p) {}
 	};
 	Node* head;
 	Node* tail;
@@ -33,8 +35,30 @@ public:
 			return ;
 		}
 		Node* temp = tail;
-		temp->next = new Node(v);
+		temp->next = new Node(v, nullptr, tail);
 		tail = temp->next;
+	}
+	
+	void removeNode(int pos){  // non-recoverable
+		if(pos == 0){        // delete head
+			Node* temp = head->next;
+			delete head;
+			head = temp;
+			head->prev = nullptr;
+			return;
+		}
+		Node* getNode = head;
+		for(int i=0; i<pos; i++)   // reach the position
+			getNode = getNode->next;
+		if(getNode == tail){    // delete tail
+			getNode->prev->next = nullptr;
+			delete getNode;
+			// why can't just getNode = nullptr ??? 
+			return;
+		}
+		getNode->prev->next = getNode->next;
+		getNode->next->prev = getNode->prev;
+		delete getNode;
 	}
 	
 	friend ostream& operator << (ostream& s, const LinkedList2& l){
@@ -46,26 +70,45 @@ public:
 		return s;
 	}
 	
-	LinkedList2 inputAndDelete(int num){
-		LinkedList2 l;
-		for(int i=1; i<=num; i++)
-			l.addEnd(i);
-		int length = num;
-		for(int i=0; i<length; i++){
-			
+	void reversePrint(){
+		Node* p = tail;
+		while(p != nullptr){
+			cout << p->val << ' ';
+			p = p->prev;
 		}
-	} 
-	
-
-
+		cout << '\n';
+	}
 };
 
-
+// after delete one element, the whole sequence changed
+LinkedList2 inputAndDelete(int num){
+	LinkedList2 l;
+	for(int i=1; i<=num; i++)
+		l.addEnd(i);
+	int length = num;
+	int deleted = 0;  // how many nodes deleted before
+	for(int i=0; i<num; i++){
+		if(i%3 == 0){
+			l.removeNode(i-deleted);
+			deleted++;
+			length--;
+		}
+	}
+	deleted = 0;
+	for(int i=0; i<length; i++){
+		if(i%3 == 0){
+			l.removeNode(i-deleted);
+			deleted++;
+		}
+	}
+	return l;
+}
 
 
 int main(){
-	LinkedList2 a;
-	for(int i=0; i<10; i++)
-		a.addEnd(i);
+	LinkedList2 a = inputAndDelete(10);
 	cout << a << '\n';
+	cout << a.getTail()->val << '\n'; 
+	cout << a.getTail()->prev->val << '\n'; 
+	//a.reversePrint();
 }
